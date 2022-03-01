18cf3d7de443da148776e488968df3852c5a1bcb
mdHasif2001 <iamhasib727@gmail.com>
2022-03-02 01:02:27 +0600

    design complete and font added

--- js/script.js
+++ js/script.js
@@ -31,17 +31,50 @@ const phoneDisplay = phones => {
         const div = document.createElement('div');
         div.classList.add('col-lg-4')
         div.classList.add('mb-5')
+        div.classList.add('col-12')
+        div.classList.add('col-md-6')
+        div.classList.add('g-5')
         div.innerHTML =
-            `
-        <div class="card" style="width: 18rem;">
-        <img src="${phone.image}" class="card-img-top" alt="...">
+        `
+        <div class="card border-0 shadow mt-5 " style="width: 18rem;">
+        <img src="${phone.image}" class="card-img-top img-fluid p-5" alt="...">
         <div class="card-body">
           <h5 class="card-title">${phone.brand}</h5>
-          <p class="card-text bold">Name: ${phone.phone_name}</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <p class="card-text">Name: ${phone.phone_name}</p>
+          <a href="#" class="btn btn-outline-primary">Details</a>
         </div>
       </div>         `
         main.appendChild(div)
     }
 }
 
+// phone detail
+const phoneDetail = (phoneId) => {
+    const url = `
+    https://openapi.programming-hero.com/api/phone/${phoneId}
+    `
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetail(data.data[0]))
+}
+
+const displayPhoneDetail = explore => {
+    console.log(explore);
+    const exploreThing = document.getElementById('phone-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML =
+        `
+    <div class="card" style="width: 18rem;">
+    <img src="${phone.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${phone.brand}</h5>
+      <p class="card-text">Name: ${phone.phone_name}</p>
+      <p class="card-text">Name: ${phone.slug}</p>
+      
+    </div>
+  </div>         
+
+    `
+    exploreThing.appendChild(div);
+}
