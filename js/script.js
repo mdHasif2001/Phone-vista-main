ebcafc4fa6b4178229539c961549d849f3d3b47d
mdHasif2001 <iamhasib727@gmail.com>
2022-03-02 19:59:16 +0600

    successfully made it into phone detail

--- js/script.js
+++ js/script.js
@@ -10,14 +10,15 @@ const searchButton = () => {
         error.innerText = "Please write something";
         input.value = "";
         main.innerHTML = "";
-
     }
+
+
     else {
         main.innerHTML = "";
         const url = ` https://openapi.programming-hero.com/api/phones?search=${inputValue}`;
         fetch(url)
             .then(res => res.json())
-            .then(data => phoneDisplay(data.data))
+            .then(data => phoneDisplay(data.data.slice(0, 20)))
 
         input.value = "";
         error.innerHTML = "";
@@ -25,9 +26,9 @@ const searchButton = () => {
 }
 
 const phoneDisplay = phones => {
-    console.log(phones);
+    // console.log(phones);
     for (const phone of phones) {
-        console.log(phone);
+        // console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col-lg-4')
         div.classList.add('mb-5')
@@ -35,46 +36,45 @@ const phoneDisplay = phones => {
         div.classList.add('col-md-6')
         div.classList.add('g-5')
         div.innerHTML =
-        `
-        <div class="card border-0 shadow mt-5 " style="width: 18rem;">
+            `
+        <div class="card border-0 shadow mt-5 g-5" style="width: 18rem;">
         <img src="${phone.image}" class="card-img-top img-fluid p-5" alt="...">
         <div class="card-body">
           <h5 class="card-title">${phone.brand}</h5>
           <p class="card-text">Name: ${phone.phone_name}</p>
-          <a href="#" class="btn btn-outline-primary">Details</a>
+          <a href="#" onclick="loadPhone('${phone.slug}')" class="btn btn-outline-primary">Details</a>
         </div>
       </div>         `
         main.appendChild(div)
     }
 }
-
 // phone detail
-const phoneDetail = (phoneId) => {
-    const url = `
-    https://openapi.programming-hero.com/api/phone/${phoneId}
-    `
+const loadPhone = (phoneId) => {
+    console.log(phoneId);
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
     fetch(url)
         .then(res => res.json())
-        .then(data => displayPhoneDetail(data.data[0]))
+        .then(data => displayPhoneDetail(data.data))
 }
 
-const displayPhoneDetail = explore => {
-    console.log(explore);
-    const exploreThing = document.getElementById('phone-details');
+const displayPhoneDetail = myPhone => {
+    console.log(myPhone);
+    const phoneDetail = document.getElementById('phone-detail');
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML =
         `
-    <div class="card" style="width: 18rem;">
-    <img src="${phone.image}" class="card-img-top" alt="...">
+    <img src="${myPhone.image}" class="card-img-top img-fluid p-5" alt="...">
     <div class="card-body">
-      <h5 class="card-title">${phone.brand}</h5>
-      <p class="card-text">Name: ${phone.phone_name}</p>
-      <p class="card-text">Name: ${phone.slug}</p>
-      
+      <h5 class="card-title">${myPhone.brand}</h5>
+      <p class="card-text">Name: ${myPhone.name}</p>
+      <p class="card-text">Chipset: ${myPhone.mainFeatures.chipSet}</p>
+      <p class="card-text">DisplaySize: ${myPhone.mainFeatures.displaySize}</p>
+      <p class="card-text">Memory: ${myPhone.mainFeatures.memory}</p>
     </div>
   </div>         
+  `
+    phoneDetail.appendChild(div)
 
-    `
-    exploreThing.appendChild(div);
+
 }
