3fa5dd90abebd4b903ecb75eb08e2fe0c4ef96bc
mdHasif2001 <iamhasib727@gmail.com>
2022-03-02 22:44:53 +0600

    almost done

--- js/script.js
+++ js/script.js
@@ -11,8 +11,17 @@ const searchButton = () => {
         input.value = "";
         main.innerHTML = "";
     }
+    if(!isNaN(inputValue)){
+        error.innerText = "Please give a valid name";
+        input.value = "";
+        main.innerHTML = "";
+    }
+    if(inputValue == false){
+        error.innerText = "Please give a valid name";
+        input.value = "";
+        main.innerHTML = "";
 
-
+    }
     else {
         main.innerHTML = "";
         const url = ` https://openapi.programming-hero.com/api/phones?search=${inputValue}`;
@@ -24,7 +33,7 @@ const searchButton = () => {
         error.innerHTML = "";
     }
 }
-
+// phone display
 const phoneDisplay = phones => {
     // console.log(phones);
     for (const phone of phones) {
@@ -37,10 +46,10 @@ const phoneDisplay = phones => {
         div.classList.add('g-5')
         div.innerHTML =
             `
-        <div class="card border-0 shadow mt-5 g-5" style="width: 18rem;">
+        <div class="card-fluid border-0 shadow mt-5 g-5" style="width: 18rem;">
         <img src="${phone.image}" class="card-img-top img-fluid p-5" alt="...">
         <div class="card-body">
-          <h5 class="card-title">${phone.brand}</h5>
+          <h5 class="card-title text-primary"><b>${phone.brand}</b></h5>
           <p class="card-text">Name: ${phone.phone_name}</p>
           <a href="#" onclick="loadPhone('${phone.slug}')" class="btn btn-outline-primary">Details</a>
         </div>
@@ -50,7 +59,7 @@ const phoneDisplay = phones => {
 }
 // phone detail
 const loadPhone = (phoneId) => {
-    console.log(phoneId);
+    // console.log(phoneId);
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
     fetch(url)
         .then(res => res.json())
@@ -58,20 +67,24 @@ const loadPhone = (phoneId) => {
 }
 
 const displayPhoneDetail = myPhone => {
-    console.log(myPhone);
+    // console.log(myPhone);
     const phoneDetail = document.getElementById('phone-detail');
     const div = document.createElement('div');
     div.classList.add('card');
+    div.classList.add('border-2');
+    div.classList.add('border-primary');
+    div.classList.add('mb-3');
+    
     div.innerHTML =
         `
-    <img src="${myPhone.image}" class="card-img-top img-fluid p-5" alt="...">
+    <img src="${myPhone.image}" class="card-img-top p-5" alt="..." >
     <div class="card-body">
-      <h5 class="card-title">${myPhone.brand}</h5>
-      <p class="card-text">Name: ${myPhone.name}</p>
-      <p class="card-text">Chipset: ${myPhone.mainFeatures.chipSet}</p>
-      <p class="card-text">DisplaySize: ${myPhone.mainFeatures.displaySize}</p>
-      <p class="card-text">Memory: ${myPhone.mainFeatures.memory}</p>
-      <p class="card-text">Sensors:<br><br> ${myPhone.mainFeatures.sensors[0]}</p>
+      <h5 class="card-title text-primary"><b>${myPhone.brand}</b></h5>
+      <p class="card-text"><b>Name</b>: </span>${myPhone.name}</p>
+      <p class="card-text"><b>Chipset</b>: ${myPhone.mainFeatures.chipSet}</p>
+      <p class="card-text"><b>DisplaySize</b>: ${myPhone.mainFeatures.displaySize}</p>
+      <p class="card-text"><b>Memory</b>: ${myPhone.mainFeatures.memory}</p>
+      <p class="card-text"><b>Sensors</b>:<br><br> ${myPhone.mainFeatures.sensors[0]}</p>
       <p class="card-text">${myPhone.mainFeatures.sensors[1]}</p>
       <p class="card-text">${myPhone.mainFeatures.sensors[2]}</p>
       <p class="card-text">${myPhone.mainFeatures.sensors[3]}</p>
@@ -80,9 +93,8 @@ const displayPhoneDetail = myPhone => {
       <h6 class="card-title">${myPhone.releaseDate}</h6>
 
     </div>
+
   </div>         
   `
-    phoneDetail.appendChild(div)
-
-
+    phoneDetail.appendChild(div);
 }
